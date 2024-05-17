ca1d9844e6c4eedea85bf376b20b0d3379175dcb
Your Name <you@example.com>
2024-05-17 16:54:17 -0700

    footer and navbar

---
+++ portfolio-website/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <div>
+                <LinkedInIcon />
+            </div>
+            <p> &copy; 2024 ameliegomez-portfolio.com</p>
+        </div>
+    );
+}
+
+export default Footer;
