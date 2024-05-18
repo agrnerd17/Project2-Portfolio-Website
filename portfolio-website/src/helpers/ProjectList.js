66c50140332484000782b3cea02857f3b21fe7ab
Your Name <you@example.com>
2024-05-17 23:30:36 -0700

    final website design

---
+++ portfolio-website/src/helpers/ProjectList.js
@@ -0,0 +1,11 @@
+import Proj1 from "../assets/e-market-trove.png";
+
+
+
+export const ProjectList = [
+    {
+        name: "E-Commerce Website",
+        image: Proj1,
+        skills: "ReactJS, Material UI, HTML, CSS",
+    }
+];
--- portfolio-website/src/pages/Projects.js
+++ portfolio-website/src/pages/Projects.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ProjectItem from "../components/ProjectItem";
-import Proj1 from "../assets/e-market-trove.png";
+import "../styles/Projects.css";
+import {ProjectList} from "../helpers/ProjectList";
 
 function Projects() {
     return (
     <div className="projects">
         <h1>My Projects</h1>
         <div className="projectList">
-            <ProjectItem name="E-Market-Trove" image={Proj1}/>
-            <ProjectItem />
+            {ProjectList.map((project, idx) => {
+                return (
+                <ProjectItem id={idx} name={project.name} image={project.image}/>
+                );
+            })}
 
         </div>
     </div> 
